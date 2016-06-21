6a578ff9eed13a3766be93e4b0b1218bfa23e508
kimspark <sir.k.spark@gmail.com>
2016-06-21 11:39:03 +0300

    text about indexes

--- DeleteMesssage/DeleteMesssage/SQL Tasks/KontraktQuery.sql
+++ DeleteMesssage/DeleteMesssage/SQL Tasks/KontraktQuery.sql
@@ -6,19 +6,20 @@ where Document.Date > DATEADD(month, -1, GetDate())
 group by Rubric.Name
 having count(Document_Rubric.ID_Document) >= 100
 
---Дополнительно можно построить индексы на все внешние ключи в таблице Document_Rubric, на Rubric.Name - так как основная группировка ведется по этому полю
+--Дополнительно можно построить индексы на все внешние ключи в таблице Document_Rubric, 
+--на Rubric.Name - так как основная группировка ведется по этому полю и дату, по которой ведется поиск
 
 --Есть схема БД
 --Customer (Id, Name)
 --Sale (Id, CustomerId, Date, Sum)
 --Написать запросы:
 --1. Лучший покупатель за все время (тот Customer, у которого суммарно больше всего поле Sum)
---2. Лучший покупатель для каждого месяца (то же самое, что и 1, только для каждого месяца текущего года)
 select TOP 1 Customer.Name,sum(Sale.Sum) all_summ
 from Customer inner join Sale on Customer.Id = Sale.CustomerId
 group by Customer.Name
 order by all_summ desc
 
+--2. Лучший покупатель для каждого месяца (то же самое, что и 1, только для каждого месяца текущего года)
 WITH Sales(Name, month_sum, SaleMonth)  
 AS  
 	(select Customer.Name,sum(Sale.Sum), month(Sale.date)
